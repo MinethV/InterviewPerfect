6529566f1caa8463248357237bc713dd1433087f
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-02-19 14:45:18 +0530

    Login page completed
    Background image changed.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import { Home } from './pages/Home';  //importing Home page from pages
 // import { Review } from './pages/Review';  //importing Home page from pages
 import { Route, Routes } from 'react-router-dom';
+import {Login} from "./pages/Login";
 
 
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
+                <Route path="/Login" element={<Login/>}/>
             </Routes>
         </>
     )
