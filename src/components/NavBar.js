0d0905e453c13ca539cb0f371aa0d228694711da
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-03-08 11:17:40 +0530

    Profile icon added to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from '../images/logo.png';
-import {Link,useMatch,useResolvedPath} from "react-router-dom";
+import {Link, useMatch, useResolvedPath} from "react-router-dom";
+
 export default function NavBar() {
     return (
         <>
             <div className="container-fluid">
-                <nav className="navbar navbar-expand-lg fixed-top navbar-light mask-custom shadow-0 ps-5" >
+                <nav className="navbar navbar-expand-lg fixed-top navbar-light mask-custom shadow-0 ps-5">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">
                             <img src={logo} alt="" height="26"
@@ -20,8 +21,11 @@ export default function NavBar() {
                             <ul className="navbar-nav ms-auto">
                                 <CustomLink className="nav-link fs-5 navLink" to="/review">Review
                                 </CustomLink>
-                                <CustomLink className="nav-item nav-link fs-5 pe-3 navLink" to="/Login">Log In </CustomLink>
+                                <CustomLink className="nav-item nav-link fs-5 pe-3 navLink" to="/Login">Log
+                                    In </CustomLink>
                                 <button type="button" className="btn btn-outline-primary">Get Started</button>
+
+                                <CustomLink to='/Profile'><i className="bi bi-person-circle ms-3 "></i></CustomLink>
                             </ul>
                         </div>
                     </div>
@@ -29,11 +33,12 @@ export default function NavBar() {
             </div>
         </>
     )
+
     function CustomLink({to, children, ...props}) {
-        const resolvedPath=useResolvedPath(to);
-        const isActive = useMatch({path:resolvedPath.pathname,end:true})
-        return(
-            <li className={isActive ? "active":""}>
+        const resolvedPath = useResolvedPath(to);
+        const isActive = useMatch({path: resolvedPath.pathname, end: true})
+        return (
+            <li className={isActive ? "active" : ""}>
                 <Link to={to} {...props}>
                     {children}
                 </Link>
