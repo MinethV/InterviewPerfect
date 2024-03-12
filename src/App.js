73bdc322d84a0cc64d30efaf653ecf214ebf87a5
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-03-12 12:27:56 +0530

    Revert "Merge branch 'Sasiru'"
    This reverts commit b5fe7ed23c3e26f1e5653358927c0a4771788fff, reversing
    changes made to ce4a95f19f08ea1697971cf79eea8b1e1d34a0ae.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import {Home} from './pages/Home';  //importing Home page from pages
+import { Home } from './pages/Home';  //importing Home page from pages
+import { Industry } from './pages/Industry';  //importing Home page from pages
 // import { Review } from './pages/Review';  //importing Home page from pages
-import {Route, Routes} from 'react-router-dom';
-import {Login} from "./pages/Login";
-import {Profile} from "./pages/Profile"
+import { Route, Routes } from 'react-router-dom';
+
+
+
 
 function App() {
-    return (
+    return(
         <>
-            <NavBar/>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
-                <Route path="/Login" element={<Login/>}/>
-                <Route path="/Profile" element={<Profile/>}/>
+                <Route path="/industry" element={<Industry/>}/>
+              
             </Routes>
         </>
     )
