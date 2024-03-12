1b9341b3dcc382512ae96ef32f9a9dcec4607cb0
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-03-12 10:31:56 +0530

    Profile page completed without background

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import { Home } from './pages/Home';  //importing Home page from pages
+import {Home} from './pages/Home';  //importing Home page from pages
 // import { Review } from './pages/Review';  //importing Home page from pages
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {Login} from "./pages/Login";
-
-
+import {Profile} from "./pages/Profile"
 
 function App() {
-    return(
+    return (
         <>
-            <NavBar />
+            <NavBar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
                 <Route path="/Login" element={<Login/>}/>
+                <Route path="/Profile" element={<Profile/>}/>
             </Routes>
         </>
     )
