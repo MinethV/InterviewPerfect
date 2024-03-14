e3752299a9e83f56400d53332d16c08d82f7dff7
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-14 09:07:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { Home } from './pages/Home';  //importing Home page from pages
 import { Industry } from './pages/Industry';  //importing Home page from pages
+import { QuestionAsk } from './pages/questionAsk';
+import { Answer } from './pages/Answer'
 // import { Review } from './pages/Review';  //importing Home page from pages
 import { Route, Routes } from 'react-router-dom';
+
 
 
 
@@ -16,6 +19,9 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
                 <Route path="/industry" element={<Industry/>}/>
+                <Route path="/askquestions" element={<QuestionAsk/>}/>
+                <Route path="/answer" element={<Answer/>}/>
+                
               
             </Routes>
         </>
