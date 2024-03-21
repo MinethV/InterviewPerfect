f730f52ff3c7875f6d9874b4ec14417b4c8b852f
siths24 <sithulimalwana@gmail.com>
2024-03-21 13:46:47 +0530

    Update Feedback.js

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,2 +1,22 @@
 import React from "react";
 import NavBar from '../components/NavBar';
+
+const Feedback = () => {
+    return (
+        <>
+            <NavBar/>
+            <div className="container-fluid">
+                <div className="congratulations">
+                    <h1>Congratulations on completing the interview simulator!</h1>
+                    <p>Your dedication and thoughtful responses showcase your skills and preparation.</p>
+                    <p>Well done! Best of luck in all your future interviews. Remember, each experience is a stepping stone to success. Keep up the excellent work!</p>
+                </div>
+            </div>
+            <div>
+                 <div className="container-bar">
+                    
+                 </div>
+            </div>
+        </>
+    )
+}
