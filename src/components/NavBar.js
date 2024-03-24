fd323e43c7c0537fa2702e3ad737ef41cd0bca6d
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-24 16:24:18 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,16 @@ import React from "react";
 import { useAuth0 } from '@auth0/auth0-react';
 import logo from '../images/logo.png';
 import './Navbar.css'
+import { Button } from "bootstrap";
 
 export const Loginbutton = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
 
     return (
         !isAuthenticated && (
-            <button onClick={() => loginWithRedirect()} className="btn btn-primary">
+            <a href="#" onClick={() => loginWithRedirect()} className="nav-link fs-5 navLink">
                 Sign In
-            </button>
+            </a>
         )
     )
 }
@@ -20,7 +21,7 @@ export default function NavBar() {
         <>
             <nav className="navbar navbar-expand-lg fixed-top navbar-light mask-custom shadow-0 ps-5" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="/">
                         <img src={logo} alt="" height="26"
                              className="d-inline-block align-text-top"/>
                     </a>
@@ -34,10 +35,17 @@ export default function NavBar() {
                             <li className="nav-item">
                                 <a className="nav-link fs-5 navLink" href="#!">Review</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item custom-li">
                                 <Loginbutton/>
                             </li>
-                            <button type="button" className="btn btn-outline-primary" > <a src="/industry">Get Started</a></button>
+                            <li className="nav-item">
+                                <a className="nav-link fs-5 navLink" href="/industry">
+                                    <button type="button" className="btn btn-outline-primary" >
+                                        Get Started
+                                    </button>
+                                    
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
