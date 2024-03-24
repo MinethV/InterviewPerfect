eee73091b968090cb8b66d32be5fb4ef7906b1b5
siths24 <sithulimalwana@gmail.com>
2024-03-24 10:55:03 +0530

    Update Feedback.js

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,7 +5,7 @@ import Confidence_level from "../components/Confidence_level";
 import Filler_words from "../components/Filler_words";
 import Time_spent from "../components/Time_spent";
 
-const Feedback = () => {
+const Feedback = (askedQuestions) => {
     return (
         <>
             <NavBar/>
@@ -26,6 +26,16 @@ const Feedback = () => {
                     <Time_spent 
                         title='Overall Answer Time'/>
                 </div>
+
+                <div className="asked-questions">
+                    <h2>Asked Questions</h2>
+                    <ul>
+                        {askedQuestions.map((question, index) => (
+                            <li key={index}>{question}</li>
+                        ))}
+                    </ul>
+                </div>
+
             </div>
             
         </>
