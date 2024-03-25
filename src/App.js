0ceb2c770adcd84e80d202774d204fc74a5cb5ef
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-25 11:26:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import { Home } from './pages/Home';  //importing Home page from pages
-import { Industry } from './pages/Industry';  //importing Home page from pages
-
+import { Home } from './pages/Home';
+import { Industry } from './pages/Industry';
 import { SoftwareEngineering } from './pages/softwareengineering';
 import { CivilEngineering } from './pages/civilengineering';
 import { UIUX } from './pages/uiux';
 import { HumanResources } from './pages/humanresources';
+import { Feedback } from './pages/Feedback';
+import { useState } from 'react'; // Import useState hook
 
-
-
-// import { Review } from './pages/Review';  //importing Home page from pages
 import { Route, Routes } from 'react-router-dom';
 
-
-
-
-
 function App() {
+    const [askedQuestions, setAskedQuestions] = useState([]); // Define state for askedQuestions
+
     return(
         <>
             <NavBar />
@@ -25,16 +21,11 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
                 <Route path="/industry" element={<Industry/>}/>
-          
-
+                <Route path="/feedback" element={<Feedback askedQuestions={askedQuestions} />}/> {/* Pass askedQuestions as prop */}
                 <Route path="/softwareengineering" element={<SoftwareEngineering/>}/>
                 <Route path="/civilengineering" element={<CivilEngineering/>}/>
                 <Route path="/uiux" element={<UIUX/>}/>
-                <Route path="/humanresources" element={<HumanResources/>}/>
-
-             
-                
-              
+                <Route path="/humanresources" element={<HumanResources setAskedQuestions={setAskedQuestions} />}/> {/* Pass setAskedQuestions as prop */}
             </Routes>
         </>
     )
