e189d2a79a50b8d60765b5358200afa8fae02354
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-25 11:26:35 +0530

    Create Time_spent.js

---
+++ src/components/Time_spent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Card.css';
+import { Circle } from 'rc-progress';
+
+export  function Time_spent({ title, body }) { // Destructure props here
+  return (
+    <div className='card-container'>
+        <div className='card-content'>
+            <div className='card-title'>
+                <h3>{title}</h3>
+                <hr></hr>
+            </div>
+            
+            <div className='card-body'>
+                <p>{body}</p>
+            </div>
+        </div>
+
+        <div className='image-container'>
+            <Circle percent={77} strokeWidth={5} strokeColor="#86f984" trailWidth={4} trailColor='#808080' />
+        </div>  
+    </div>
+  );
+}
+
