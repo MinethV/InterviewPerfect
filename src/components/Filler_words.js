97ee836a48f13157b9702f6de2f4c5c2e9c868ff
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-25 11:26:32 +0530

    Create Filler_words.js

---
+++ src/components/Filler_words.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Card.css';
+import { Circle } from 'rc-progress';
+
+export function Filler_words({ title, body }) { // Destructure props here
+  return (
+    <div className='card-container'>
+        <div className='card-content'>
+            <div className='card-title'>
+                <h3>{title}</h3>
+                <hr></hr>
+            </div>
+            
+            <div className='card-body'>
+                <p>{body}</p>
+            </div>
+        </div>
+
+        <div className='image-container'>
+            <Circle percent={77} strokeWidth={5} strokeColor="#ff8788" trailWidth={4} trailColor='#808080' />
+        </div>  
+    </div>
+  );
+}
+
