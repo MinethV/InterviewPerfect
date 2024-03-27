c4c063918634ffc664b25c2444940b2525db85f4
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-27 11:26:59 +0530

    Revert "Merge branch 'main' into Final-Website"
    This reverts commit cebd3179f6e4c6d879655919c4911ce3169607c3, reversing
    changes made to 4cdca3ad7dc120c96d02c555542e4172c7ad2b16.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,6 @@ export default function NavBar() {
 
     return (
         <>
-
             <div className="container-fluid">
                 <nav className="navbar navbar-expand-lg fixed-top navbar-light mask-custom shadow-0 ps-5">
                     <div className="container-fluid">
@@ -31,29 +30,6 @@ export default function NavBar() {
                                     <CustomLink className="ms-2 btn btn-primary" to="/profile">Profile</CustomLink>}
                             </ul>
                         </div>
-
-            <nav className="navbar navbar-expand-lg fixed-top navbar-light mask-custom shadow-0 ps-5" >
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt="" height="6"
-                             className="d-inline-block align-text-top"/>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link fs-5 navLink" href="#!">Review</a>
-                            </li>
-                            <li className="nav-item">
-                                <Loginbutton/>
-                            </li>
-                            <button type="button" className="btn btn-outline-primary">Get Started</button>
-                        </ul>
-
                     </div>
                 </nav>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,6 @@ export const Home = () => {
 
     return (
         <>
-
             <div className="container-fluid homeContainer">
                 <div className={`row mb-5 ${isMobile ? "flex-column" : ""}`}>
                     <div className={`col headingCol ${isMobile ? "order-2" : ""}`}>
@@ -48,16 +47,6 @@ export const Home = () => {
                         <button type="button" className="btn btn-primary" onClick={handleVisit}>
                             Get Started
                         </button>
-
-            <div className="container-fluid">
-                <div className="row mb-5" >
-                    <div className="col headingCol">
-                        <p className="lh-1 heading">Interview <br/> mastery, <br/> made easy.</p>
-                        <p className="fs-5 fw-light">Never fear an interview again: Prepare <br/> 
-                            for success with our comprehensive <br/>
-                            and versatile interview simulator</p>
-                        <button type="button" className="btn btn-primary">Get Started</button>
-
                     </div>
                     <div className={`col homeImage ${isMobile ? "order-1" : ""}`}>
                         <img src={HomeImg} className="img-fluid" id="IntImg1" alt=""/>
