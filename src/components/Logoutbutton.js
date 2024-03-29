603b1698538ec2d0228713079c320bcb6996d9e2
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-30 04:12:58 +0530

    Update Logoutbutton.js

--- src/components/Logoutbutton.js
+++ src/components/Logoutbutton.js
@@ -1,16 +1,12 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import {useAuth0} from "@auth0/auth0-react";
 
-export const LogoutButton = () =>  {
-    const { logout, isAuthenticated } = useAuth0();
-
+export const LogoutButton = () => {
+    const {logout, isAuthenticated} = useAuth0();
     return (
         isAuthenticated && (
-            <button onClick={() => logout()}>
-                Sign Out
+            <button onClick={() => logout()} className='btn btn-primary'>
+                Log Out
             </button>
         )
     )
 }
-
-
-
