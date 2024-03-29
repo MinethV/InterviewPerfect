23783be2401f2f1cfd451b3bda2d0b710314af0c
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-30 04:20:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer'
 import { Home } from './pages/Home';  //importing Home page from pages
 import { Industry } from './pages/Industry';  //importing Home page from pages
-// import { Review } from './pages/Review';  //importing Home page from pages
+import Callback from './Callback';
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -17,7 +17,7 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Home/>}/>
                 <Route path="/industry" element={<Industry/>}/>
-              
+                <Route path="/callback" element={<Callback/>}/>
             </Routes>
             <Footer />
         </>
